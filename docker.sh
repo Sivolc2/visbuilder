2276121696e04bfe016fcd9060ce594f3fc3eb80
cvt <clovisvt@gmail.com>
2025-01-29 08:50:33 -0800

    lightsail test

---
+++ docker.sh
@@ -0,0 +1,2 @@
+docker build -t flask-container .
+docker run -p 5003:5003 flask-container
---
+++ lightsail.sh
@@ -0,0 +1,3 @@
+# https://aws.amazon.com/tutorials/serve-a-flask-app/
+aws lightsail create-container-service --service-name flask-service --power small --scale 1
+aws lightsail get-container-services
